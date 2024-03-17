ea511aa39b79b8ba3852fcecd55be7db334ea62d
Felipe Augusto <augustto.felipe@hotmail.com>
2024-03-17 21:13:19 +0000

    chore: update personal info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Felipe Augusto",
+  title: "Software/Data Guy & Entrepreneur",
+  email: "augustto.felipe@hotmail.com",
+  gitHub: "f2acode",
+  // instagram: "microsoft",
+  linkedIn: "felipe-augustox",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  // twitter: "microsoft",
+  // youTube: "Code",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,19 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Computer scientist 👨‍🔬 and Master in modeling complex systems (USP). Self-taught with experience in front end, back end, mobile and data engineering.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Team management",
+  "Software development",
+  "Data engineering",
+  "Software architecture",
+  "Software design",
+  "Entrepreneurship",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.";
 
 const About = () => {
   return (
